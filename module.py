fc67505bc949f888652dbaa2967980a9e7a3947a
Артем Карманчик <artemkarmancik@gmail.com>
2026-08-04 00:44:24 +0500

    Day 1: add module.py

---
+++ module.py
@@ -0,0 +1,10 @@
+print("Привет! Это мой первый файл module.py")
+
+name = input("Как тебя зовут? ")
+age = input("Сколько тебе лет? ")
+
+age = int(age)
+
+print(f"Привет, {name}!")
+print(f"Тебе {age} лет.")
+print(f"Через 5 лет тебе будет {age + 5}.")
